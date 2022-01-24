1542184e13727321336407d64e1479bd05df2e5e
hwanggiju <84834776+hwanggiju@users.noreply.github.com>
2022-01-24 16:47:16 +0900

    1/24
    modify

--- test.py
+++ test.py
@@ -7,10 +7,10 @@ if cap.isOpened() :
     delay = int(1000 / cap.get(cv2.CAP_PROP_FPS))
     while True :
         ret, frame = cap.read()
-        frame = cv2.resize(frame, (0, 0), fx = 0.25, fy = 0.25)
+        small_frame = cv2.resize(frame, (0, 0), fx = 0.25, fy = 0.25)
         if ret :
-            frame = cv2.flip(frame, 1)
-            cv2.imshow("Video", frame)
+            small_frame = cv2.flip(small_frame, 1)
+            cv2.imshow("Video", small_frame)
             if cv2.waitKey(delay) & 0xFF == 27:
                 break
             
