6ef68a01b6bbece21a43d4c80d73562bb9414a65
hwanggiju <84834776+hwanggiju@users.noreply.github.com>
2022-05-02 15:22:49 +0900

    main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# 캡스톤 디자인 최종 코드 작성 부분
+# 필요한 라이브러리 선언
 import face_recognition
 import cv2
 import time
@@ -6,11 +6,14 @@ from scipy.spatial import distance as dist
 import sys
 import numpy as np
 
+# 졸음 인식 판단 카운트 값
 EYES_CLOSED_SECONDS = 5
 
+# 카페 학습된 이미지 모델 선언
 model = 'res10_300x300_ssd_iter_140000.caffemodel'
 config = 'deploy.prototxt.txt'
 
+# 네트워크 구성
 net = cv2.dnn.readNet(model, config)
 
 if net.empty() :
@@ -18,27 +21,27 @@ if net.empty() :
     sys.exit()
 
 def main():
-    closed_count = 0
+    closed_count = 0    # 카운트 비교 변수 선언
     cap = cv2.VideoCapture(0)
     _, frame = cap.read(0)
-    print(cap.get(cv2.CAP_PROP_FRAME_WIDTH), cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    print(cap.get(cv2.CAP_PROP_FRAME_WIDTH), 
+          cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
     
     if not cap.isOpened() :
         print('Camera open failed!')
         sys.exit()
-        
+    
     small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
     rgb_small_frame = small_frame[:, :, ::-1]
     face_landmarks_list = face_recognition.face_landmarks(rgb_small_frame)
-    process = True
     
+    process = True    
+    
+    # 사용자 인식 구현 부분 --------------------------------------------------------
     while True:
         _, frame = cap.read(0)
         if frame is None :
             break
-
-        small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
-        rgb_small_frame = small_frame[:, :, ::-1]
         
         blob = cv2.dnn.blobFromImage(frame, 1, (300, 300), (104, 177, 123))
         net.setInput(blob)
@@ -60,34 +63,36 @@ def main():
             cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0))
             
             # 책상 다리 모터 제어에 활용되는 값
-            area = (x2-x1) * (y2-y1)
+            area = (x2-x1) * (y2-y1) # 사용자 인식 넓이
             center_x = x1 + (x2-x1)/2 
-            center_y = y1 + (y2-y1)/2
+            center_y = y1 + (y2-y1)/2 # 인식된 부분 중심 좌표 x, y 값
             
             cv2.imshow('Facerec_Video', frame)
-
-            if process:
+            
+            print('area : %d    center_x : %d   center_y : %d' 
+                %(area, center_x, center_y))
+        # -----------------------------------------------------------------------------
+            ret, frame = cap.read(0)
+            small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+            rgb_small_frame = small_frame[:, :, ::-1]
+            
+            if process :
                 face_landmarks_list = face_recognition.face_landmarks(rgb_small_frame)
-                
+            
+            # 사용자 눈 인식 구현 부분 ----------------------------------------
                 for face_landmark in face_landmarks_list:
                     left_eye = face_landmark['left_eye']
                     right_eye = face_landmark['right_eye']
-                    color = (255,0,0)
-                    thickness = 2
-
-                    cv2.rectangle(small_frame, left_eye[0], right_eye[-1], color, thickness)
-
-                    cv2.imshow('Sleep_Video', small_frame)
 
                     ear_left = get_ear(left_eye)
                     ear_right = get_ear(right_eye)
                     
-                    print('area : %d    center_x : %d   center_y : %d   left_eye : %lf   left_eye : %lf' 
-                          %(area, center_x, center_y, ear_left, ear_right))
+                    print('left_eye : %lf   left_eye : %lf' 
+                        %(ear_left, ear_right))
 
-                    # 졸음 방지 진동 모터에 활용될 값
-                    closed = ear_left < 0.2 and ear_right < 0.2
+                    # 졸음 방지 알림에 활용될 코드
                     closed = 0
+                    closed = ear_left < 0.2 and ear_right < 0.2
                     if (closed):
                         closed_count += 1
 
@@ -103,20 +108,19 @@ def main():
                                 asleep = False
                                 print("EYES OPENED")
                         closed_count = 0
-
-        process = not process
+            # -------------------------------------------------------------
+        
         key = cv2.waitKey(1) & 0xFF
         if key == 27:
             break
 
-
+# 특징점 좌표값을 받은 후 거리값 도출 함수
 def get_ear(eye):
 	A = dist.euclidean(eye[1], eye[5])
 	B = dist.euclidean(eye[2], eye[4])
 	C = dist.euclidean(eye[0], eye[3])
 	ear = (A + B) / (2.0 * C)
 	return ear
-
 
 if __name__ == "__main__":
     main()
