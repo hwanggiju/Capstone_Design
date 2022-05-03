84f8ca41bb1a27684064cb5c2a1127ab14d1b33a
hyu-nani <cube_h2@naver.com>
2022-05-03 17:55:13 +0900

    Update test1.py

--- test1.py
+++ test1.py
@@ -38,7 +38,7 @@ def main():
         sys.exit()
     
 
-    rgb_small_frame = small_frame[:, :, ::-1]
+    #rgb_small_frame = small_frame[:, :, ::-1]
     face_landmarks_list = face_recognition.face_landmarks(rgb_small_frame)
     
     #process = True    
