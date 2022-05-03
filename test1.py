9335f9052b3435154eaa5f934f7dfbb6a002eeef
hyu-nani <cube_h2@naver.com>
2022-05-03 17:48:03 +0900

    Update test1.py

--- test1.py
+++ test1.py
@@ -58,7 +58,7 @@ def main():
         for box in results:
             # 좌표 추출
             x, y, w, h = box
-        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 255, 255), thickness=2)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 255, 255), thickness=2)
         #blob 사람인식 속도
         net.setInput(blob)
         '''
