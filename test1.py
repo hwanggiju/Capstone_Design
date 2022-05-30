393d822517cd8063630ba80a185ce541a73c9650
hwanggiju <84834776+hwanggiju@users.noreply.github.com>
2022-05-30 21:46:28 +0900

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,55 +1,59 @@
-import RPi.GPIO as gpio
+import RPi.GPIO as GPIO
 import time
 
-gpio.setmode(gpio.BOARD)
-gpio.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
 
-gpio.setup(33, gpio.OUT) # enA
-gpio.setup(11, gpio.OUT) # enB
-gpio.setup(31, gpio.OUT) # in1
-gpio.setup(29, gpio.OUT) # in2
-gpio.setup(15, gpio.OUT) # in3
-gpio.setup(13, gpio.OUT) # in4
+GPIO.setup(33, GPIO.OUT) # enA
+GPIO.setup(11, GPIO.OUT) # enB
+GPIO.setup(31, GPIO.OUT) # in1
+GPIO.setup(29, GPIO.OUT) # in2
+GPIO.setup(15, GPIO.OUT) # in3
+GPIO.setup(13, GPIO.OUT) # in4
 
 
 while True :
-    gpio.output(33, gpio.LOW) # enA
-    gpio.output(11, gpio.LOW) # enB
-    gpio.output(31, gpio.LOW) # in1
-    gpio.output(29, gpio.LOW) # in2
-    gpio.output(15, gpio.LOW) # in3
-    gpio.output(13, gpio.LOW) # in4
+    GPIO.output(33, GPIO.LOW) # enA
+    GPIO.output(11, GPIO.LOW) # enB
+    
+    GPIO.output(31, GPIO.LOW) # in1
+    GPIO.output(29, GPIO.LOW) # in2
+    GPIO.output(15, GPIO.LOW) # in3
+    GPIO.output(13, GPIO.LOW) # in4
 
     time.sleep(0.2)
 
-    gpio.output(33, gpio.HIGH)
-    gpio.output(11, gpio.HIGH)
-    gpio.output(31, gpio.HIGH)
-    gpio.output(29, gpio.LOW)
-    gpio.output(15, gpio.HIGH)
-    gpio.output(13, gpio.LOW)
+    GPIO.output(33, GPIO.HIGH)
+    GPIO.output(11, GPIO.HIGH)
+    
+    GPIO.output(31, GPIO.HIGH)
+    GPIO.output(29, GPIO.LOW)
+    GPIO.output(15, GPIO.HIGH)
+    GPIO.output(13, GPIO.LOW)
     
     time.sleep(1)
     
-    gpio.output(33, gpio.LOW)
-    gpio.output(11, gpio.LOW)
-    gpio.output(31, gpio.LOW)
-    gpio.output(29, gpio.LOW)
-    gpio.output(15, gpio.LOW)
-    gpio.output(13, gpio.LOW)
+    GPIO.output(33, GPIO.LOW)
+    GPIO.output(11, GPIO.LOW)
+    
+    GPIO.output(31, GPIO.LOW)
+    GPIO.output(29, GPIO.LOW)
+    GPIO.output(15, GPIO.LOW)
+    GPIO.output(13, GPIO.LOW)
 
     time.sleep(0.2)
 
-    gpio.output(33, gpio.HIGH)
-    gpio.output(11, gpio.HIGH)
-    gpio.output(31, gpio.LOW)
-    gpio.output(29, gpio.HIGH)
-    gpio.output(15, gpio.LOW)
-    gpio.output(13, gpio.HIGH)
+    GPIO.output(33, GPIO.HIGH)
+    GPIO.output(11, GPIO.HIGH)
+    
+    GPIO.output(31, GPIO.LOW)
+    GPIO.output(29, GPIO.HIGH)
+    GPIO.output(15, GPIO.LOW)
+    GPIO.output(13, GPIO.HIGH)
     
     
     time.sleep(1)
     
     if KeyboardInterrupt :
-        gpio.cleanup()
+        GPIO.cleanup()
     
