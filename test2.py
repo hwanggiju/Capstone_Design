ae01b3d2a05c4604e9b4187ef5f8ee4f8969e530
hyu-nani <cube_h2@naver.com>
2022-05-30 22:26:45 +0900

    Update test2.py

--- test2.py
+++ test2.py
@@ -10,33 +10,27 @@ in2 = 5
 in3 = 22
 in4 = 27
 GPIO.cleanup()
-#GPIO.setup(enA, GPIO.OUT) # enA
-#GPIO.setup(enB, GPIO.OUT) # enB
-#GPIO.setup(in1, GPIO.OUT) # in1
-#GPIO.setup(in2, GPIO.OUT) # in2
-#GPIO.setup(in3, GPIO.OUT) # in3
-#GPIO.setup(in4, GPIO.OUT) # in4
+GPIO.setup(enA, GPIO.OUT) # enA
+GPIO.setup(enB, GPIO.OUT) # enB
+GPIO.setup(in1, GPIO.OUT) # in1
+GPIO.setup(in2, GPIO.OUT) # in2
+GPIO.setup(in3, GPIO.OUT) # in3
+GPIO.setup(in4, GPIO.OUT) # in4
 
 
 while True :
-    GPIO.setup(enA, GPIO.OUT)  # enA
     GPIO.output(enA, GPIO.LOW) # enA
-    GPIO.setup(enB, GPIO.OUT)  # enB
     GPIO.output(enB, GPIO.LOW) # enB
 
-    GPIO.setup(in1, GPIO.OUT)  # in1
     GPIO.output(in1, GPIO.LOW) # in1
-    GPIO.setup(in2, GPIO.OUT)  # in2
     GPIO.output(in2, GPIO.LOW) # in2
-    GPIO.setup(in3, GPIO.OUT)  # in3
     GPIO.output(in3, GPIO.LOW) # in3
-    GPIO.setup(in4, GPIO.OUT)  # in4
     GPIO.output(in4, GPIO.LOW) # in4
 
     time.sleep(0.2)
 
-    GPIO.output(enA, GPIO.HIGH)
-    GPIO.output(enB, GPIO.HIGH)
+    GPIO.output(enA, 0)
+    GPIO.output(enB, 0)
     
     GPIO.output(in1, GPIO.HIGH)
     GPIO.output(in2, GPIO.LOW)
