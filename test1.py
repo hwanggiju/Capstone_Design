870495340440dc61dd9c3ea14f21aeb01e0d1c48
hwanggiju <84834776+hwanggiju@users.noreply.github.com>
2022-05-31 00:09:53 +0900

    Update test1.py

--- test1.py
+++ test1.py
@@ -15,6 +15,26 @@ GPIO.setup(15, GPIO.OUT)
 GPIO.setup(29, GPIO.OUT)
 GPIO.setup(31, GPIO.OUT)
 
+GPIO.output(13, GPIO.LOW)
+GPIO.output(15, GPIO.LOW)
+GPIO.output(29, GPIO.LOW)
+GPIO.output(31, GPIO.LOW)
+
+GPIO.output(11, GPIO.LOW)
+GPIO.output(33, GPIO.LOW)
+
+time.sleep(0.2)
+
+GPIO.output(13, GPIO.LOW)
+GPIO.output(15, GPIO.HIGH)
+GPIO.output(29, GPIO.LOW)
+GPIO.output(31, GPIO.HIGH)
+
+GPIO.output(11, GPIO.HIGH)
+GPIO.output(33, GPIO.HIGH)
+
+time.sleep(2)
+
 try :
     while 1 :
         GPIO.output(13, GPIO.LOW)
