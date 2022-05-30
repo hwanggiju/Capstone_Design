dff7fe39a1a2839201b5516b125f2267032d6dde
hwanggiju <84834776+hwanggiju@users.noreply.github.com>
2022-05-30 23:32:01 +0900

    Update test1.py

--- test1.py
+++ test1.py
@@ -4,7 +4,7 @@ import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
-en_lst = [11, 33]
+en_lst = [11, 33] 
 in13_lst = [13, 29]
 in24_lst = [15, 31]
 
@@ -14,31 +14,43 @@ GPIO.setup(in24_lst, GPIO.OUT)
 
 try :
     while 1 :
-        GPIO.output(in13_lst, GPIO.LOW)
-        GPIO.output(in24_lst, GPIO.LOW)
+        GPIO.output(13, GPIO.LOW)
+        GPIO.output(15, GPIO.LOW)
+        GPIO.output(29, GPIO.LOW)
+        GPIO.output(31, GPIO.LOW)
         
-        GPIO.output(en_lst, GPIO.LOW)
+        GPIO.output(11, GPIO.LOW)
+        GPIO.output(33, GPIO.LOW)
 
         time.sleep(0.2)
         
-        GPIO.output(in13_lst, GPIO.HIGH) #
-        GPIO.output(in24_lst, GPIO.LOW)
+        GPIO.output(13, GPIO.HIGH)
+        GPIO.output(15, GPIO.LOW)
+        GPIO.output(29, GPIO.HIGH)
+        GPIO.output(31, GPIO.LOW)
         
-        GPIO.output(en_lst, GPIO.HIGH)
+        GPIO.output(11, GPIO.HIGH)
+        GPIO.output(33, GPIO.HIGH)
         
         time.sleep(1)
         
-        GPIO.output(in13_lst, GPIO.LOW)
-        GPIO.output(in24_lst, GPIO.LOW)
+        GPIO.output(13, GPIO.LOW)
+        GPIO.output(15, GPIO.LOW)
+        GPIO.output(29, GPIO.LOW)
+        GPIO.output(31, GPIO.LOW)
         
-        GPIO.output(en_lst, GPIO.LOW)
+        GPIO.output(11, GPIO.LOW)
+        GPIO.output(33, GPIO.LOW)
 
         time.sleep(0.2)
         
-        GPIO.output(in13_lst, GPIO.LOW)
-        GPIO.output(in24_lst, GPIO.HIGH)
+        GPIO.output(13, GPIO.HIGH)
+        GPIO.output(15, GPIO.LOW)
+        GPIO.output(29, GPIO.HIGH)
+        GPIO.output(31, GPIO.LOW)
         
-        GPIO.output(en_lst, GPIO.HIGH)
+        GPIO.output(11, GPIO.HIGH)
+        GPIO.output(33, GPIO.HIGH)
         
         time.sleep(1)
     
