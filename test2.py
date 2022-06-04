cee56b2cd2d95d74a5737e9b3a747703cc9eba27
hyu-nani <cube_h2@naver.com>
2022-06-05 02:30:46 +0900

    a

--- test2.py
+++ test2.py
@@ -6,21 +6,24 @@ cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
 if cap.isOpened() :
-    print("Camera Is Opened")
-    delay = int(1000 / cap.get(cv2.CAP_PROP_FPS))
-    while True :
-        ret, frame = cap.read()
-        if ret :
-            img_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            img_gray = cv2.flip(img_gray, 1)
-            cv2.imshow("Video", img_gray)
-            if cv2.waitKey(delay) & 0xFF == 27:
-                print("ESC Key pressed")
+    try:
+        print("Camera Is Opened")
+        delay = int(1000 / cap.get(cv2.CAP_PROP_FPS))
+        while True :
+            ret, frame = cap.read()
+            if ret :
+                img_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                img_gray = cv2.flip(img_gray, 1)
+                cv2.imshow("Video", img_gray)
+                if cv2.waitKey(delay) & 0xFF == 27:
+                    print("ESC Key pressed")
+                    break
+
+            else :
+                print(ret, frame)
                 break
-            
-        else :
-            print(ret, frame)
-            break
+    except:
+        pass
     
 else :
     print("Camera Isn't Opened")
