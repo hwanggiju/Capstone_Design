29fbc2ab3f7cba6eb5a14d691da0ce598ad728bd
hwanggiju <84834776+hwanggiju@users.noreply.github.com>
2022-06-06 00:57:06 +0900

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,27 +1,26 @@
 import cv2
 
 cap = cv2.VideoCapture(0)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
 if cap.isOpened() :
-    try:
-        print("Camera Is Opened")
-        while True :
-            ret, frame = cap.read()
-            print(ret, frame)
+    print("Camera Is Opened")
+    delay = int(1000 / cap.get(cv2.CAP_PROP_FPS))
+    while True :
+        ret, frame = cap.read()
+        if ret :
             img_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
             img_gray = cv2.flip(img_gray, 1)
             cv2.imshow("Video", img_gray)
             if cv2.waitKey(delay) & 0xFF == 27:
                 print("ESC Key pressed")
                 break
-    except:
-        pass
+            
+        else :
+            print(ret, frame)
+            break
     
 else :
     print("Camera Isn't Opened")
     
 cap.release()
 cv2.destroyAllWindows()
-#
