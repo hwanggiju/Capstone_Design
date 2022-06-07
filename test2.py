afb601f9ac498785681e95bfa2db9eb358490818
hwanggiju <84834776+hwanggiju@users.noreply.github.com>
2022-06-07 16:42:10 +0900

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,3 +1,19 @@
+import smbus			#import SMBus module of I2C
+from time import sleep          #import
+from time import time 
+
+PWR_MGMT_1   = 0x6B
+SMPLRT_DIV   = 0x19
+CONFIG       = 0x1A
+GYRO_CONFIG  = 0x1B
+INT_ENABLE   = 0x38
+ACCEL_XOUT_H = 0x3B
+ACCEL_YOUT_H = 0x3D
+ACCEL_ZOUT_H = 0x3F
+GYRO_XOUT_H  = 0x43
+GYRO_YOUT_H  = 0x45
+GYRO_ZOUT_H  = 0x47
+
 # 각도(deg) = Gyro값(step) / DEGREE_PER_SECOND(step*sec/deg) * dt(sec) 의 누적...
 DEGREE_PER_SECOND = 32767 / 250  # Gyro의 Full Scale이 250인 경우
                                  # Full Scale이 1000인 경우 32767/1000
@@ -13,6 +29,36 @@ baseGyZ = 0
 GyX_deg = 0   # 측정 각도
 GyY_deg = 0
 GyZ_deg = 0
+
+def get_raw_data():
+    """
+    가속도(accel)와 각속도(gyro)의 현재 값 읽기
+    :return: accel x/y/z, gyro x/y/z
+    """
+    gyro_xout = read_word_2c(GYRO_XOUT_H)
+    gyro_yout = read_word_2c(GYRO_YOUT_H)
+    gyro_zout = read_word_2c(GYRO_ZOUT_H)
+    accel_xout = read_word_2c(ACCEL_XOUT_H)
+    accel_yout = read_word_2c(ACCEL_YOUT_H)
+    accel_zout = read_word_2c(ACCEL_ZOUT_H)
+    return accel_xout, accel_yout, accel_zout,\
+           gyro_xout, gyro_yout, gyro_zout
+
+def cal_angle_acc(AcX, AcY, AcZ):
+    """
+    Accel값만 이용해서 X, Y의 각도 측정
+    (고정 좌표 기준?)
+    그런데... 각도가 0 -> 90 -> 0 -> -90 -> 0으로 바뀐다. 왜?
+    0도 -> 90도 -> 180도 -> 270도 -> 360도
+    즉, 30도와 120도가 모두 30도로 표시된다. 왜?
+    :param AcX: Accel X
+    :param AcY: Accel Y
+    :param AcZ: Accel Z
+    :return: X, Y angle in degree
+    """
+    y_radians = math.atan2(AcX, math.sqrt((AcY*AcY) + (AcZ*AcZ)))
+    x_radians = math.atan2(AcY, math.sqrt((AcX*AcX) + (AcZ*AcZ)))
+    return math.degrees(x_radians), -math.degrees(y_radians)
 
 def cal_angle_gyro(GyX, GyY, GyZ):
     """
@@ -31,6 +77,7 @@ def cal_angle_gyro(GyX, GyY, GyZ):
     GyZ_deg += ((GyZ - baseGyZ) / DEGREE_PER_SECOND) * dt
 
     return now      # 다음 계산을 위해 past로 저장되어야 한다.
+
 
 def sensor_calibration():
     """
@@ -63,37 +110,27 @@ def sensor_calibration():
 
     return avgAcX, avgAcY, avgAcZ, avgGyX, avgGyY, avgGyZ
 
-# Now wake the 6050 up as it starts in sleep mode
-# MPU 6050 초기화
-def set_MPU6050_init(dlpf_bw=DLPF_BW_256,
-                     gyro_fs=GYRO_FS_250, accel_fs=ACCEL_FS_2,
-                     clk_pll=CLOCK_PLL_XGYRO):
-    global baseAcX, baseAcY, baseAcZ, baseGyX, baseGyY, baseGyZ, past
+def MPU_Init():
+    	#write to sample rate register
+	bus.write_byte_data(Device_Address, SMPLRT_DIV, 7)
+	
+	#Write to power management register
+	bus.write_byte_data(Device_Address, PWR_MGMT_1, 1)
+	
+	#Write to Configuration register
+	bus.write_byte_data(Device_Address, CONFIG, 0)
+	
+	#Write to Gyro configuration register
+	bus.write_byte_data(Device_Address, GYRO_CONFIG, 24)
+	
+	#Write to interrupt enable register
+	bus.write_byte_data(Device_Address, INT_ENABLE, 1)
 
-    # MPU6050 초기값 setting
-    write_byte(PWR_MGMT_1, SLEEP_EN | clk_pll)      # sleep mode(bit6), clock(bit2:0)은 XGyro 동기
-    write_byte(CONFIG, dlpf_bw)                     # bit 2:0
-    write_byte(GYRO_CONFIG, gyro_fs)                # Gyro Full Scale bit 4:3
-    write_byte(ACCEL_CONFIG, accel_fs)              # Accel Full Scale Bit 4:3
-    write_byte(PWR_MGMT_1, SLEEP_DIS | clk_pll)     # Start
-
-    # sensor 계산 초기화
-    baseAcX, baseAcY, baseAcZ, baseGyX, baseGyY, baseGyZ \
-        = sensor_calibration()
-    past = time.time()
-
-    return read_byte(PWR_MGMT_1)    # 정말 시작되었는지 확인
-
-########################################
-# 테트스 코드
-########################################
 if __name__ == '__main__':
     ''' -----------------------------------'''
     ''' Gyro 테스트 '''
     ''' -----------------------------------'''
-    # 1) MPU 6050 초기화
-    test = set_MPU6050_init(dlpf_bw=DLPF_BW_98)   # BW만 변경, 나머지는 default 이용
-    print("Gyro PWR_MGMT_1 Register = ", test)
+    MPU_Init()
 
     # 2) Gyro 기준값 계산(Gyro 이용시)
     sensor_calibration()    # Gyro의 기준값 계산
@@ -118,9 +155,6 @@ if __name__ == '__main__':
         if cnt%100 == 0:
             print("GyX,Y,Z_deg = ", GyX_deg, ',', GyY_deg, ',', GyZ_deg)
             # print("AcX_deg, AcY_deg = ", AcX_deg, ',', AcY_deg)
-
-
-
 '''
 def read_raw_data(addr):
 	#Accelero and Gyro value are 16-bit
@@ -166,4 +200,4 @@ while True:
 	
 
 	print ("Gx=%.2f" %Gx, u'\u00b0'+ "/s", "\tGy=%.2f" %Gy, u'\u00b0'+ "/s", "\tGz=%.2f" %Gz, u'\u00b0'+ "/s", "\tAx=%.2f g" %Ax, "\tAy=%.2f g" %Ay, "\tAz=%.2f g" %Az) 	
-'''
+	'''
