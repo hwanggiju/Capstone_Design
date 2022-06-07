ebebd50bf5dacbcbc282cc36273362faa399f522
hwanggiju <84834776+hwanggiju@users.noreply.github.com>
2022-06-07 11:08:20 +0900

    Update smartdesk.py

--- smartdesk.py
+++ smartdesk.py
@@ -308,7 +308,7 @@ def main():
         print("초음파 측정 거리 : %d" % (waveSensorHeight))
         print ("Gyro x: {0} ; Gyro y : {1} ; Gyro z : {2}".format(imu.GyroVals[0], imu.GyroVals[1], imu.GyroVals[2]))
         
-        cv2.imshow('Facerec_Video', rotate_frame)
+        # cv2.imshow('Facerec_Video', rotate_frame)
         key = cv2.waitKey(1) & 0xFF
         if key == 27:
             # enA_pwm.stop()
