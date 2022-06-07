0d8091d1d7580ea2c88a473057261750d3a7bcc3
hwanggiju <84834776+hwanggiju@users.noreply.github.com>
2022-06-07 10:47:34 +0900

    Update smartdesk.py

--- smartdesk.py
+++ smartdesk.py
@@ -5,20 +5,6 @@ import RPi.GPIO as GPIO
 import time
 import sys
 import numpy as np
-from mpu9250_jmdev.registers import *
-from mpu9250_jmdev.mpu_9250 import MPU9250
-
-mpu = MPU9250(
-    address_ak=AK8963_ADDRESS, 
-    address_mpu_master=MPU9050_ADDRESS_68, # In 0x68 Address
-    address_mpu_slave=None, 
-    bus=1,
-    gfs=GFS_1000, 
-    afs=AFS_8G, 
-    mfs=AK8963_BIT_16, 
-    mode=AK8963_MODE_C100HZ)
-
-mpu.configure() # mpu 레지스터 설정
 
 # spi SSD1306 OLED code
 # pip install spidev 설치
@@ -319,7 +305,6 @@ def main():
                     stop = False
 
         print("초음파 측정 거리 : %d" % (waveSensorHeight))
-        print("Gyroscope", mpu.readGyroscopeMaster()) # 자이로
         
         cv2.imshow('Facerec_Video', rotate_frame)
         key = cv2.waitKey(1) & 0xFF
