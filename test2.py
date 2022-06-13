e4f5ff1d22d137900080b0940a57ea394e9e9c50
hwanggiju <84834776+hwanggiju@users.noreply.github.com>
2022-06-13 16:37:28 +0900

    Update test2.py

--- test2.py
+++ test2.py
@@ -7,44 +7,31 @@ $ git clone https://github.com/adafruit/Adafruit_Python_SSD1306.git
 $ cd Adafruit_Python_SSD1306
 $ sudo python3 setup.py install
 '''
-# oled 라이브러리
-import Adafruit_GPIO.SPI as SPI
-import Adafruit_SSD1306
-# PIL image 라이브러리
-from PIL import Image
-from PIL import ImageDraw
-from PIL import ImageFont
-import spidev
+import board
+import digitalio
+from PIL import Image, ImageDraw, ImageFont
+import adafruit_ssd1306
 
-# oled display 오브젝트 셋업
-RST = 23
-DC = 24
-SPI_PORT = 0
-SPI_DEVICE = 0
+# Change these
+# to the right size for your display!
+WIDTH = 128
+HEIGHT = 64  # Change to 64 if needed
+BORDER = 5
 
-disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST, dc=DC, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE, max_speed_hz=8000000))
-disp.begin()
+# Use for SPI
+spi = board.SPI()
+oled_reset = digitalio.DigitalInOut(board.D25)
+oled_cs = digitalio.DigitalInOut(board.D8)
+oled_dc = digitalio.DigitalInOut(board.D24)
+oled = adafruit_ssd1306.SSD1306_SPI(WIDTH, HEIGHT, spi, oled_dc, oled_reset, oled_cs)
 
-# clear display
-disp.clear()
-disp.display()
+# Clear display.
+oled.fill(0)
+oled.show()
 
-# 빈 이미지 준비하기
-width = disp.width
-height = disp.height
-image = Image.new('1',(width, height)) # 1bit-black&white  이미지이므로 1.
-draw = ImageDraw.Draw(image)
+# Alternatively load a different format image, resize it, and convert to 1 bit color.
+image = Image.open('spi_test_img.png').resize((oled.width, oled.height), Image.ANTIALIAS).convert('1')
 
-#font 준비
-font = ImageFont.truetype("malgun.ttf",15)
-
-# draw a text
-draw.text(
-(10,10),
-'''세종대왕
-만만세''',
-font=font, fill=255)
-
-# oled에 보이기
-disp.image(image)
-disp.display()
+# Display image.
+oled.image(image)
+oled.show()
