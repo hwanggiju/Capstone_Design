fdd2298e0114af8240ed482bf0c2597c81bbde29
hwanggiju <84834776+hwanggiju@users.noreply.github.com>
2022-06-14 17:12:24 +0900

    Update test2.py

--- test2.py
+++ test2.py
@@ -4,13 +4,13 @@ import digitalio
 from PIL import Image, ImageDraw, ImageFont
 import adafruit_ssd1306
 import os
-# import time
+import time
 
 WIDTH = 128
 HEIGHT = 64 
 BORDER = 5
 
-# SET_HEIGHT = 170
+SET_HEIGHT = 170
 
 # Use for SPI
 spi = board.SPI()
@@ -27,30 +27,30 @@ font = ImageFont.load_default()
 image = Image.new('1', (oled.width, oled.height), 255)
 draw = ImageDraw.Draw(image)
 
-# def OLED_initial_setting_Height(CHANGE_HEIGHT) :
-draw.text((5, 0), 'First Setting', font = font, fill = 0)
-draw.text((5, 20), 'Input your Height', font = font, fill = 0)
-#draw.text((5, 40), str(SET_HEIGHT), font = font, fill = 0)
-oled.image(image)
-oled.show()
+def OLED_initial_setting_Height(CHANGE_HEIGHT) :
+    draw.text((5, 0), 'First Setting', font = font, fill = 0)
+    draw.text((5, 20), 'Input your Height', font = font, fill = 0)
+    #draw.text((5, 40), str(SET_HEIGHT), font = font, fill = 0)
+    oled.image(image)
+    oled.show()
 
-# OLED_initial_setting_Height(SET_HEIGHT)     
+OLED_initial_setting_Height(SET_HEIGHT)     
 
-'''
 while True :
     if digitalio.DigitalInOut(board.D16) == 1 :
         SET_HEIGHT = SET_HEIGHT + 5
         OLED_initial_setting_Height(SET_HEIGHT)
         oled.image(image)
-        oled.show() 
+        oled.show()
+        time.sleep(0.2)
     elif digitalio.DigitalInOut(board.D20) == 1:
         SET_HEIGHT = SET_HEIGHT - 5
         OLED_initial_setting_Height(SET_HEIGHT)
         oled.image(image)
-        oled.show() 
+        oled.show()
+        time.sleep(0.2)
     elif digitalio.DigitalInOut(board.D21) == 1:
         SET_HEIGHT = SET_HEIGHT
         oled.fill(0)
         oled.show()
         break
-'''
