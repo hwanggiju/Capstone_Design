ab69e6e866dc911dfa95f556666e7cce5eb62042
HyeonHan Bae <60097661+hyu-nani@users.noreply.github.com>
2022-06-14 14:02:56 +0900

    update test1.py

--- test1.py
+++ test1.py
@@ -4,6 +4,7 @@ import RPi.GPIO as GPIO
 # GPIO 번호 사용
 switch =  [36, 38, 40] # -> 실제 핀 번호[36, 38, 40]
 GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
 
 
 GPIO.setup(switch[0], GPIO.IN)
@@ -17,13 +18,13 @@ down_btn = GPIO.input(switch[2])
 try:
     print('button test')
     while 1 :
-        if up_btn == 1 :
+        if GPIO.input(switch[0]) == 1 :
             print('u')
             
-        elif down_btn == 1 :
+        elif GPIO.input(switch[1]) == 1 :
             print('d')
         
-        elif okay_btn == 1 :
+        elif GPIO.input(switch[2]) == 1 :
             print('o')
             
 except KeyboardInterrupt :
