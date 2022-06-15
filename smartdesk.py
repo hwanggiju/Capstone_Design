2b1fa010d99f93b9734bec835b019a79aec7b993
hwanggiju <84834776+hwanggiju@users.noreply.github.com>
2022-06-16 00:10:20 +0900

    Update smartdesk.py

--- smartdesk.py
+++ smartdesk.py
@@ -624,7 +624,7 @@ def main():
             detect = net.forward()
             (h, w) = rotate_frame.shape[:2]
             detect = detect[0, 0, :, :]
-            
+            print(fixAngle)
             waveSensorHeight = waveFun() # 책상 높이
             WaveAVG[0] = waveSensorHeight
             for i in range(len(WaveAVG) - 1) :
