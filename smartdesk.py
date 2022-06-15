787dddc4ecbc0bd2c8a02e95060a963882354d88
hwanggiju <84834776+hwanggiju@users.noreply.github.com>
2022-06-15 23:51:23 +0900

    Update smartdesk.py

--- smartdesk.py
+++ smartdesk.py
@@ -436,11 +436,11 @@ def HorizontalHold(nowAngle, compareAngle):
     pwmB = 80
     # diffPwm = int(np.sin((nowAngle-compareAngle) / 1.2 * 90 * np.pi/180) * 30)
     if (nowAngle-compareAngle) < 0 :
-        pwmA = 20 * np.cos((nowAngle-compareAngle) * np.pi/180) + pwmA
-        pwmB = 20 * (1-np.cos((nowAngle-compareAngle) * np.pi/180)) + pwmB
+        pwmA = (20 - 5 * (1-np.cos((nowAngle-compareAngle) * np.pi/180))) * np.cos((nowAngle-compareAngle) * np.pi/180) + pwmA
+        pwmB = (20 - 5 * np.cos((nowAngle-compareAngle) * np.pi/180)) * (1-np.cos((nowAngle-compareAngle) * np.pi/180)) + pwmB
     elif (nowAngle-compareAngle) > 0:
-        pwmA = (20 - 5 * (1-np.cos((nowAngle-compareAngle) * np.pi/180))) * (1-np.cos((nowAngle-compareAngle) * np.pi/180)) + pwmA
-        pwmB = (20 - 5 * np.cos((nowAngle-compareAngle) * np.pi/180)) * np.cos((nowAngle-compareAngle) * np.pi/180) + pwmB
+        pwmA = (20 - 5 * np.cos((nowAngle-compareAngle) * np.pi/180)) * (1-np.cos((nowAngle-compareAngle) * np.pi/180)) + pwmA
+        pwmB = (20 - 5 * (1-np.cos((nowAngle-compareAngle) * np.pi/180))) * np.cos((nowAngle-compareAngle) * np.pi/180) + pwmB
     else :
         pwmA = pwmA
         pwmB = pwmB
