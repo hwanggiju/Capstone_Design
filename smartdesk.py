a76ac9aed954edaadcfba91487f56892697093db
hwanggiju <84834776+hwanggiju@users.noreply.github.com>
2022-06-16 08:19:10 +0900

    Update smartdesk.py

--- smartdesk.py
+++ smartdesk.py
@@ -625,7 +625,7 @@ def main():
             detect = detect[0, 0, :, :]
             
             draw.text((5, 0), 'Desk Tall', font = font, fill = 255)
-            draw.text((5, 20), str(waveSensorMean+3), font = font, fill = 255)
+            draw.text((5, 20), '%d'%(str(waveSensorMean+3)), font = font, fill = 255)
             draw.text((110, 0), 'up', font = font2, fill = 255)
             draw.text((110, 30), 'okay', font = font2, fill = 255)
             draw.text((110, 45), 'down', font = font2, fill = 255)
@@ -633,7 +633,7 @@ def main():
             draw.text((110, 30), 'okay', font = font2, fill = 0)
             draw.text((110, 60), 'down', font = font2, fill = 0)
             draw.text((5, 0), 'Desk Tall', font = font, fill = 0)
-            draw.text((5, 20), str(waveSensorMean+3), font = font, fill = 0)
+            draw.text((5, 20), '%d'%(str(waveSensorMean+3)), font = font, fill = 0)
             oled.image(image)
             oled.show()
             
