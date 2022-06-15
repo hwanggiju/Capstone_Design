dc8bae9e8047b479845415d46ef4adf286fd3261
hwanggiju <84834776+hwanggiju@users.noreply.github.com>
2022-06-16 05:20:40 +0900

    Update smartdesk.py

--- smartdesk.py
+++ smartdesk.py
@@ -436,7 +436,7 @@ def HorizontalHold(nowAngle, compareAngle, waveSensorMean):
     pwmB = 0
     diffPwmA = 100 - int(10 * np.sin(abs(nowAngle-compareAngle) * 45 * np.pi/180))
     diffPwmB = 100 - int(10 * np.cos(abs(nowAngle-compareAngle) * 45 * np.pi/180))
-    print(abs(nowAngle-compareAngle) * np.pi/180)
+    print(abs(nowAngle-compareAngle) *45* np.pi/180)
     if actionPre == 2 :
         if (nowAngle < compareAngle) : # enA가 enB보다 빨라야한다. 올라갈 때 기준이다.  얼마나 빨라야하는가? enA에 보상을 준다. enB에게는 채찍을 준다
             pwmA = diffPwmA
