92f1fc956dab61e43a245e52435f4ece5b6d7420
hwanggiju <84834776+hwanggiju@users.noreply.github.com>
2022-06-16 07:07:02 +0900

    Update smartdesk.py

--- smartdesk.py
+++ smartdesk.py
@@ -321,7 +321,7 @@ def calGyro(accelX, accelY, accelZ, GyroAccX, GyroAccY, GyroAccZ):
     accelAngleY = math.atan(-1 * accelX / math.sqrt(math.pow(accelY, 2) + math.pow(accelZ, 2))) * RADIANS_TO_DEGREES
     accelAngleZ = 0
     # complementary Filter
-    alpha = 0.86
+    alpha = 0.89
     GyX_deg = alpha * gyroAngleX + (1.0 - alpha) * accelAngleX
     GyY_deg = alpha * gyroAngleY + (1.0 - alpha) * accelAngleY
     GyZ_deg = gyroAngleZ
