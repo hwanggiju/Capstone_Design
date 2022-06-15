938d1665982fcdd21676006d7da7eaf903451fe1
hwanggiju <84834776+hwanggiju@users.noreply.github.com>
2022-06-16 00:56:23 +0900

    Update smartdesk.py

--- smartdesk.py
+++ smartdesk.py
@@ -434,28 +434,33 @@ def changePWM(enA, enB):
 def HorizontalHold(nowAngle, compareAngle):
     fixPwmA = 0
     fixPwmB = 0
-    pwmA = 0
-    pwmB = 0
+    pwmA = 80
+    pwmB = 80
+    i = 1
     # diffPwm = int(np.sin((nowAngle-compareAngle) / 1.2 * 90 * np.pi/180) * 30)
-    for i in range(1, 21) :
-        if actionPre == 2 :
-            pwmA = 80
-            pwmB = 80
-            if fixPwmA == 0 and fixPwmB == 0:
-                if nowAngle < compareAngle : # enA가 enB보다 빨라야한다. 올라갈 때 기준이다.  얼마나 빨라야하는가? enA에 보상을 준다. enB에게는 채찍을 준다
-                    pwmA = pwmA + i
-                    pwmB = pwmB
-                    changePWM(pwmA, pwmB)
-                elif nowAngle > compareAngle: # enA가 enB보다 느려야한다. 올라갈 때 기준이다. 반대
-                    pwmA = pwmA
-                    pwmB = pwmB + i
-                    changePWM(pwmA, pwmB)
-                else :
-                    fixPwmA = pwmA
-                    fixPwmB = pwmB
-                    changePWM(fixPwmA, fixPwmB)
+    if actionPre == 2 :
+        if fixPwmA == 0 and fixPwmB == 0:
+            if nowAngle < compareAngle : # enA가 enB보다 빨라야한다. 올라갈 때 기준이다.  얼마나 빨라야하는가? enA에 보상을 준다. enB에게는 채찍을 준다
+                pwmA = pwmA + i
+                pwmB = pwmB
+                i += 1
+                if i == 20 :
+                    i = 1
+                changePWM(pwmA, pwmB)
+            elif nowAngle > compareAngle: # enA가 enB보다 느려야한다. 올라갈 때 기준이다. 반대
+                pwmA = pwmA
+                pwmB = pwmB + i
+                i += 1
+                if i == 20 :
+                    i = 1
+                changePWM(pwmA, pwmB)
+                changePWM(pwmA, pwmB)
             else :
+                fixPwmA = pwmA
+                fixPwmB = pwmB
                 changePWM(fixPwmA, fixPwmB)
+        else :
+            changePWM(fixPwmA, fixPwmB)
     print(str(fixPwmA) + " / " + str(fixPwmB))
         
     '''
