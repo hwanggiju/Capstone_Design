374f01668621eb5119b5c0f860e2c09a997332f2
hwanggiju <84834776+hwanggiju@users.noreply.github.com>
2022-06-16 05:03:10 +0900

    Update smartdesk.py

--- smartdesk.py
+++ smartdesk.py
@@ -432,29 +432,30 @@ def changePWM(enA, enB):
 
 #각도 자세유지 코드
 def HorizontalHold(nowAngle, compareAngle, waveSensorMean):
-    pwmA = 85
-    pwmB = 85
-    diffPwm = int(np.sin((nowAngle-compareAngle) / 1.2 * 90 * np.pi/180) * 15)
+    pwmA = 0
+    pwmB = 0
+    diffPwmA = 100 - int(10 * np.sin(abs(nowAngle-compareAngle) / 1.2 * 90 * np.pi/180))
+    diffPwmB = 100 - int(10 * np.cos(abs(nowAngle-compareAngle) / 1.2 * 90 * np.pi/180))
     if actionPre == 2 :
         if (nowAngle < compareAngle) : # enA가 enB보다 빨라야한다. 올라갈 때 기준이다.  얼마나 빨라야하는가? enA에 보상을 준다. enB에게는 채찍을 준다
-            pwmA = pwmA + diffPwm
-            pwmB = pwmB - diffPwm
+            pwmA = diffPwmA
+            pwmB = diffPwmB
         elif nowAngle > compareAngle: # enA가 enB보다 느려야한다. 올라갈 때 기준이다. 반대
-            pwmA = pwmA - diffPwm
-            pwmB = pwmB + diffPwm
+            pwmA = diffPwmA
+            pwmB = diffPwmB
         else :          # 값이 서로 비슷해지면 pwm 값을 유지하면서 올라간다
             pwmA = pwmA
             pwmB = pwmB
     elif actionPre == 0 :
         if (nowAngle < compareAngle) : 
-            pwmA = pwmA + diffPwm
-            pwmB = pwmB - diffPwm
+            pwmA = diffPwmA
+            pwmB = diffPwmB
         elif (nowAngle > compareAngle):
-            pwmA = pwmA - diffPwm
-            pwmB = pwmB + diffPwm
+            pwmA = diffPwmA
+            pwmB = diffPwmB
         else :
-            pwmA = pwmA
-            pwmB = pwmB
+            pwmA = diffPwmA
+            pwmB = diffPwmB
     changePWM(pwmA, pwmB)
     print(str(pwmA) + '/' + str(pwmB))
     return i
