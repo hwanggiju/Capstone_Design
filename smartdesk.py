0d39bd16ab4bbe20d791dfb850bf060f610eaec1
hwanggiju <84834776+hwanggiju@users.noreply.github.com>
2022-06-16 08:50:08 +0900

    Update smartdesk.py

--- smartdesk.py
+++ smartdesk.py
@@ -434,8 +434,8 @@ def changePWM(enA, enB):
 def HorizontalHold(nowAngle, compareAngle, waveSensorMean):
     pwmA = 0
     pwmB = 0
-    diffPwmA = int(100 * np.sin((90 + abs(nowAngle-compareAngle)) * np.pi/180))
-    diffPwmB = int(100 * np.cos((90 - abs(nowAngle-compareAngle)) * np.pi/180))
+    diffPwmA = int(100 * np.sin((45 + abs(nowAngle-compareAngle)) * np.pi/180))
+    diffPwmB = int(100 * np.cos((45 - abs(nowAngle-compareAngle)) * np.pi/180))
     if actionPre == 2 :
         if (nowAngle < compareAngle) : 
             pwmA = diffPwmA
