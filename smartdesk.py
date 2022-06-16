67a2faa21d5d832d6fa17e15806ea086391309d1
hyu-nani <cube_h2@naver.com>
2022-06-16 14:16:26 +0900

    Update smartdesk.py

--- smartdesk.py
+++ smartdesk.py
@@ -742,7 +742,7 @@ def main():
                 ani = FuncAnimation(plt.gcf(), animate, interval=10)
                 plt.tight_layout()
                 plt.show()
-                display.clear_output(wait=True)
+                display.clear_output(wait=False)
                 # 실제 책상 높이는 78cm인데, 키를 바탕으로한 최적의 높이 식을 대입하면 키가 190cm 사람이 최적의 책상 높이가 77.9 ????
                 # 책상의 최적 높이와 사용자의 현재 키를 빼서 최적의 값을 알아낸다 
                 #높이에 따른 모터작동
