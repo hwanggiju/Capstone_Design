eaf6819d04fdb9aecc66fb997ad8f563ead2a011
hyu-nani <cube_h2@naver.com>
2022-06-16 19:05:27 +0900

    Update smartdesk.py

--- smartdesk.py
+++ smartdesk.py
@@ -342,8 +342,8 @@ Ki_term = 0
 # 출력 > PID값
 def PID(currentVal,setVal):
     global pastPID, preError, Kp_term, Ki_term, Kd_term
-    Kp = 15.7 #비례
-    Ki = 7.11 #적분
+    Kp = 5.7 #비례
+    Ki = 17.11 #적분
     Kd = 10.5 #미분
     now = time.time()
     dt = (now - pastPID) / 1.0
