5f1bb50931f8b834d783ff908bc1f2129ed153dc
hyu-nani <cube_h2@naver.com>
2022-06-16 13:53:18 +0900

    그래프

--- smartdesk.py
+++ smartdesk.py
@@ -13,6 +13,16 @@ import adafruit_ssd1306
 import os
 import smbus
 from imusensor.MPU9250 import MPU9250
+#그래프
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+x_val = [i for i in range(100)]
+y_val = [0 for i in range(100)]
+def animate(, i):
+    plt.cla()
+    plt.plot(x_val, y_val)
+    for i in range(len(y_val)-1):
+        y_val[len(y_val) - i - 1] = y_val[len(y_val) - i - 2]
 '''
 해야할 것
 - 정확한 각도 도출
@@ -645,6 +655,11 @@ def main():
         stop = False
         HeightAVG = [130 for i in range(30)]
         WaveAVG = [waveSensorHeight for i in range(15)]
+
+        ani = FuncAnimation(plt.gcf(), animate, interval=1)
+        plt.tight_layout()
+        plt.show()
+
         while True:
             time.sleep(0.005)
             nowTime = time.time()
@@ -723,6 +738,9 @@ def main():
                 print("차값 :" + str(userHeight - userHeightAVG))
                 val = PID(userHeightAVG, userHeight)
                 print("PID 계산값 " + str(round(val, 5)))
+                # 그래프
+                y_val[0] = userHeightAVG
+
                 # 실제 책상 높이는 78cm인데, 키를 바탕으로한 최적의 높이 식을 대입하면 키가 190cm 사람이 최적의 책상 높이가 77.9 ????
                 # 책상의 최적 높이와 사용자의 현재 키를 빼서 최적의 값을 알아낸다 
                 #높이에 따른 모터작동
