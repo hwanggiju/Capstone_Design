0088d458bd2c410e99d1e55cb0dabc44bed304fe
hyu-nani <cube_h2@naver.com>
2022-06-16 16:53:04 +0900

    Update smartdesk.py

--- smartdesk.py
+++ smartdesk.py
@@ -341,9 +341,9 @@ Ki_term = 0
 # 출력 > PID값
 def PID(currentVal,setVal):
     global pastPID, preError, Kp_term, Ki_term, Kd_term
-    Kp = 1
-    Ki = 1
-    Kd = 1
+    Kp = 0.4 #비례
+    Ki = 0.1 #적분
+    Kd = 1.2 #미분
     now = time.time()
     dt = (now - pastPID) / 1.0
     errorGap_P = setVal - currentVal
@@ -766,11 +766,13 @@ def main():
                     y_val[0] = userHeight
                     y_valAVG[0] = userHeightAVG
                     y_valPID[0] = val
+                    y_valDesk[0] = waveSensorHeight + 2
                     # 쉬프트 그래프
                     for i in range(graphRow - 1):
                         y_val[graphRow - i - 1] = y_val[graphRow - i - 2]
                         y_valPID[graphRow - i - 1] = y_valPID[graphRow - i - 2]
                         y_valAVG[graphRow - i - 1] = y_valAVG[graphRow - i - 2]
+                        y_valDesk[graphRow - i - 1] = y_valDesk[graphRow - i - 2]
                 # 실제 책상 높이는 78cm인데, 키를 바탕으로한 최적의 높이 식을 대입하면 키가 190cm 사람이 최적의 책상 높이가 77.9 ????
                 # 책상의 최적 높이와 사용자의 현재 키를 빼서 최적의 값을 알아낸다 
                 #높이에 따른 모터작동
@@ -802,7 +804,7 @@ def main():
                         stop = False
             print("초음파 측정 거리 : %d\n" % (waveSensorMean+3))
             #그래프 표시
-            y_valDesk[0] = waveSensorHeight + 2
+
             gyrosensorX[0] = angleX
             gyrosensorY[0] = angleY
             # 쉬프트
@@ -811,7 +813,7 @@ def main():
                 gyrosensorY[graphRow - i - 1] = gyrosensorY[graphRow - i - 2]
                 ENA_PWM[graphRow - i - 1] = ENA_PWM[graphRow - i - 2]
                 ENB_PWM[graphRow - i - 1] = ENB_PWM[graphRow - i - 2]
-                y_valDesk[graphRow - i - 1] = y_valDesk[graphRow - i - 2]
+
             if TESTMODE == True:
                 line1.set_ydata(y_val)
                 line2.set_ydata(y_valAVG)
