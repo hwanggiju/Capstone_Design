e67f699f84ac96f04a42d38dc56f9ac69f0973c3
hwanggiju <84834776+hwanggiju@users.noreply.github.com>
2022-06-17 18:49:50 +0900

    Update smartdesk.py

--- smartdesk.py
+++ smartdesk.py
@@ -826,18 +826,18 @@ def main():
                 # 책상의 최적 높이와 사용자의 현재 키를 빼서 최적의 값을 알아낸다 
                 
                 if GPIO.input(switch[2]) == 1 :
-                    stop = driverSet(100, 2, 2, 100)
+                    btn_stop = driverSet(100, 2, 2, 100)
                 
                 if GPIO.input(switch[0]) == 1 :
-                    stop = driverSet(100, 1, 1, 100)
+                    btn_stop = driverSet(100, 1, 1, 100)
                     
-                if stop == True :
+                if btn_stop == True :
                     if GPIO.input(switch[2]) == 0 :
-                        stop = driverSet(0, 0, 0, 0)
-                        stop = False
+                        btn_stop = driverSet(0, 0, 0, 0)
+                        btn_stop = False
                     if GPIO.input(switch[0]) == 0 :
-                        stop = driverSet(0, 0, 0, 0)
-                        stop = False
+                        btn_stop = driverSet(0, 0, 0, 0)
+                        btn_stop = False
                         
                 if ((waveSensorMean+3) >= deskUserTall) and actionPre == 2 :
                     stop = driverSet(0, 0, 0, 0)
