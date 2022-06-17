85b8d745e09fe74504a3977e1392dc464d1a3730
hwanggiju <84834776+hwanggiju@users.noreply.github.com>
2022-06-17 22:07:53 +0900

    Update smartdesk.py

--- smartdesk.py
+++ smartdesk.py
@@ -888,7 +888,7 @@ def main():
             
             if GPIO.input(switch[2]) == 1 :
                 if btn_stop != True :
-                        btn_stop = driverSet(100, 2, 2, 100)
+                    btn_stop = driverSet(100, 2, 2, 100)
                     
             elif GPIO.input(switch[2]) == 0 :
                 btn_stop = driverSet(0, 0, 0, 0)
@@ -896,7 +896,7 @@ def main():
                     
             if GPIO.input(switch[0]) == 1 :
                 if btn_stop != True :
-                        btn_stop = driverSet(100, 1, 1, 100)
+                    btn_stop = driverSet(100, 1, 1, 100)
                     
             elif GPIO.input(switch[0]) == 0 :
                 btn_stop = driverSet(0, 0, 0, 0)
