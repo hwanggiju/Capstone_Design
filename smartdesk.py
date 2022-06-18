bc19acc845ff578e89ee2e070f0c942a441b69e5
hwanggiju <84834776+hwanggiju@users.noreply.github.com>
2022-06-18 18:02:57 +0900

    Update smartdesk.py

--- smartdesk.py
+++ smartdesk.py
@@ -650,12 +650,14 @@ def eraseDisplay() :
     oled.show()
     
 # 최적높이 재설정 모드 display erase 
+prechange = 0
 def ReSetMode(NowHeight, changeHeight) :
     global timeTest, nowTime, preTime
     if timeTest == True :
         preTime = nowTime
         timeTest = False
     if nowTime - preTime > 0.001 :
+        eraseReSetMode(NowHeight, prechange)
         draw.text((100, 0), 'Up', font=font2, fill=0)
         draw.text((100, 20), 'Mode', font=font2, fill=0)
         draw.text((100, 40), 'Down', font=font2, fill=0)
@@ -665,7 +667,7 @@ def ReSetMode(NowHeight, changeHeight) :
         draw.text((5, 30), '-Change Tall-', font=font2, fill=0)
         draw.text((5, 45), str(int(changeHeight)), font=font2, fill=0)
         draw.text((40, 45), 'cm', font = font2, fill = 0)
-        eraseReSetMode(NowHeight, changeHeight)
+        prechange = changeHeight
         oled.image(image)
         oled.show()
 
