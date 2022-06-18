78308dad9179573af2305cd2df7e1b423ef59ba8
hwanggiju <84834776+hwanggiju@users.noreply.github.com>
2022-06-18 19:23:45 +0900

    Update smartdesk.py

--- smartdesk.py
+++ smartdesk.py
@@ -912,7 +912,7 @@ def main():
                 # 책상의 최적 높이와 사용자의 현재 키를 빼서 최적의 값을 알아낸다 
                     
                 #높이에 따른 모터작동
-                if stop != True and addcontrol != False: # 드라이버 pin Set 변경 후 반복 변경 방지
+                if stop != True and addcontrol == False: # 드라이버 pin Set 변경 후 반복 변경 방지
                     # 앉았을 때, 책상의 최적 높이 설정
                     # down
                     if userHeightAVG < 140 :
