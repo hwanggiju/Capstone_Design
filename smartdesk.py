c8c76437f9d422b0d90d66f0cd3cfa231379b10e
hyu-nani <cube_h2@naver.com>
2022-06-20 03:17:20 +0900

    Update smartdesk.py

--- smartdesk.py
+++ smartdesk.py
@@ -1032,7 +1032,7 @@ def main():
                         recognitionMode[2] = True
                         stop = False
                 if recognitionMode[2] == True: # 모터 작동 모드
-                    if waveSensorMean > deskMoveTall - 1 and waveSensorMean < deskMoveTall + 1:
+                    if waveSensorMean >= deskMoveTall - 1 and waveSensorMean <= deskMoveTall + 1:
                         pwmA_AVG = 0
                         pwmB_AVG = 0
                         fixAngleY = angleY  # 현재 각도고정
@@ -1040,7 +1040,7 @@ def main():
                         stop = driverSet(0, 0, 0, 0)
                         recognitionMode[2] = False
                         recognitionMode[0] = True
-                    elif waveSensorHeight < deskMoveTall + 5 and stop == False: # 설정키보다 작다면
+                    elif waveSensorHeight < deskMoveTall - 1 and stop == False: # 설정키보다 작다면
                         pwmA_AVG = 0
                         pwmB_AVG = 0
                         fixAngleY = angleY  # 현재 각도고정
@@ -1048,7 +1048,7 @@ def main():
                         stop = driverSet(0, 2, 2, 0)
                         actionPre = 2  # down
                         Ki_term = 0
-                    elif waveSensorHeight > deskMoveTall - 5 and stop == False: #설정키보다 크다면
+                    elif waveSensorHeight > deskMoveTall + 1 and stop == False: #설정키보다 크다면
                         pwmA_AVG = 0
                         pwmB_AVG = 0
                         fixAngleY = angleY  # 현재 각도고정
