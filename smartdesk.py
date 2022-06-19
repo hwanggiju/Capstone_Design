12095f05b7ce15ac64eeffd63f261444c8f18cbd
hyu-nani <cube_h2@naver.com>
2022-06-19 23:22:53 +0900

    Update smartdesk.py

--- smartdesk.py
+++ smartdesk.py
@@ -1007,9 +1007,9 @@ def main():
                 # 큰 움직임이 있을 때 모터 작동으로 변경
                 if abs(userHeightAVG - userHeight) > 3 and moveEnable == False:
                     moveEnable = True
-                    if userHeight < 140 and deskUserTall < SET_HEIGHT - 40:
+                    if userHeight > 140 and deskUserTall < SET_HEIGHT - 40:
                         deskUserTall = 100
-                    elif userHeight >= 140 and deskUserTall >= SET_HEIGHT - 70:
+                    elif userHeight <= 140 and deskUserTall >= SET_HEIGHT - 70:
                         deskUserTall = 70
                 if moveEnable == True:
                     if waveSensorHeight + 2 < deskUserTall and stop == False: # 설정키보다 작다면
