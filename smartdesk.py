b3fac43e42728bf08710ef7f85b9c6aa484873bb
hyu-nani <cube_h2@naver.com>
2022-06-20 01:23:10 +0900

    Update smartdesk.py

--- smartdesk.py
+++ smartdesk.py
@@ -1003,7 +1003,7 @@ def main():
                 # 책상의 최적 높이와 사용자의 현재 키를 빼서 최적의 값을 알아낸다
 
                 # 큰 움직임이 있을 때 모터 작동으로 변경 / 현재 높이에서 20CM 오차가 발생시 실행
-                if abs(userHeightAVG - (waveSensorHeight + 60)) > 20 and moveEnable == False:
+                if abs(userHeightAVG - (waveSensorHeight + 60)) > 10 and moveEnable == False:
                     moveEnable = True
                     stop = False
                     if userHeightAVG - (waveSensorHeight + 60) < 0:
