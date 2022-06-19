7fdfa0d40c282320ab52e5fdacd6ae75cdbbab46
hyu-nani <cube_h2@naver.com>
2022-06-20 00:01:15 +0900

    Update smartdesk.py

--- smartdesk.py
+++ smartdesk.py
@@ -1027,7 +1027,7 @@ def main():
                         fixAngleX = angleX
                         Ki_term = 0
                     elif (waveSensorMean + 3) > deskUserTall - 2 and (waveSensorMean + 3) < deskUserTall + 2 or (waveSensorMean) < 73:
-                        driverSet(0, 0, 0, 0)
+                        stop = driverSet(0, 0, 0, 0)
                         fixAngleY = angleY  # 현재 각도고정
                         fixAngleX = angleX
                         moveEnable == False
