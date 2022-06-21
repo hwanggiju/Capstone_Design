dbdd19e7c44f2318cac5f56d9a80636e9c2eabf3
hyu-nani <cube_h2@naver.com>
2022-06-21 15:46:45 +0900

    Update smartdesk.py

--- smartdesk.py
+++ smartdesk.py
@@ -1102,7 +1102,7 @@ def main():
                         pwmA = 100
                         pwmB = 100
                         stop = driverSet(10,1,2,10)
-                    elif stop == True and abs(angleY - fixAngleY) < 0.5:
+                    elif abs(angleY - fixAngleY) < 0.5:
                         stop = driverSet(10, 0, 0, 10)
                         recognitionMode[3] = False
                         recognitionMode[0] = True
